2fafb550c840c992518aab835a5b57c679a10b44
kaiseixd <1050207224@qq.com>
2019-09-19 03:20:44 +0800

    fix: text indent

--- history/browserHistory.js
+++ history/browserHistory.js
@@ -5,64 +5,64 @@ const listenrs = [];
 const globalHistory = window.history;
 
 class BrowserHistory {
-	constructor() {
-		bindEvent.call(this);
-	}
+    constructor() {
+        bindEvent.call(this);
+    }
 
-	location = getDOMLocation();
+    location = getDOMLocation();
 
-	push(path, state) {
-		const solvedPath = createPath(location);
-		const { state: solvedState } = createLocation(path, state);
-		globalHistory.pushState(solvedState, null, solvedPath);
-	}
+    push(path, state) {
+        const solvedPath = createPath(location);
+        const { state: solvedState } = createLocation(path, state);
+        globalHistory.pushState(solvedState, null, solvedPath);
+    }
 
-	replace(path, state) {
-		const solvedPath = createPath(location);
-		const { state: solvedState } = createLocation(path, state);
-		globalHistory.replaceState(solvedState, null, solvedPath);
-	}
+    replace(path, state) {
+        const solvedPath = createPath(location);
+        const { state: solvedState } = createLocation(path, state);
+        globalHistory.replaceState(solvedState, null, solvedPath);
+    }
 
-	go(n) {
-		globalHistory.go(n);
-	}
+    go(n) {
+        globalHistory.go(n);
+    }
 
-	goBack() {
-		this.go(-1);
-	}
+    goBack() {
+        this.go(-1);
+    }
 
-	goForward() {
-		this.go(1);
-	}
+    goForward() {
+        this.go(1);
+    }
 
-	listen(listener) {
-		return appendListener(listener);
-	}
+    listen(listener) {
+        return appendListener(listener);
+    }
 }
 
 function bindEvent() {
-	window.addEventListener('popstate', handlePopState.bind(this));
+    window.addEventListener('popstate', handlePopState.bind(this));
 }
 
 function appendListener(fn) {
-	let isActive = true;
+    let isActive = true;
 
     function listener(...args) {
-      if (isActive) fn(...args);
+        if (isActive) fn(...args);
     }
 
     listeners.push(listener);
 
     return () => {
-      isActive = false;
-      listeners = listeners.filter(item => item !== listener);
+        isActive = false;
+        listeners = listeners.filter(item => item !== listener);
     };
 }
 
 function handlePopState(event) {
-	const location = getDOMLocation(event.state);
-	Object.assign(this.location, location)
-	listenrs.forEach(listenr => listenr(this.location));
+    const location = getDOMLocation(event.state);
+    Object.assign(this.location, location)
+    listenrs.forEach(listenr => listenr(this.location));
 }
 
 export default BrowserHistory;
--- history/locationUtils.js
+++ history/locationUtils.js
@@ -1,42 +1,42 @@
 import { parsePath } from './pathUtils';
 
 export function createLocation(path, state) {
-  let location;
-  if (typeof path === 'string') {
-    // Two-arg form: push(path, state)
-    location = parsePath(path);
-    location.state = state;
-  } else {
-    // One-arg form: push(location)
-    location = { ...path };
-
-    if (location.pathname === undefined) location.pathname = '';
-
-    if (location.search) {
-      if (location.search.charAt(0) !== '?')
-        location.search = '?' + location.search;
+    let location;
+    if (typeof path === 'string') {
+        // Two-arg form: push(path, state)
+        location = parsePath(path);
+        location.state = state;
     } else {
-      location.search = '';
+        // One-arg form: push(location)
+        location = { ...path };
+
+        if (location.pathname === undefined) location.pathname = '';
+
+        if (location.search) {
+            if (location.search.charAt(0) !== '?')
+                location.search = '?' + location.search;
+        } else {
+            location.search = '';
+        }
+
+        if (location.hash) {
+            if (location.hash.charAt(0) !== '#') location.hash = '#' + location.hash;
+        } else {
+            location.hash = '';
+        }
+
+        if (state !== undefined && location.state === undefined)
+            location.state = state;
     }
 
-    if (location.hash) {
-      if (location.hash.charAt(0) !== '#') location.hash = '#' + location.hash;
-    } else {
-      location.hash = '';
-    }
-
-    if (state !== undefined && location.state === undefined)
-      location.state = state;
-  }
-
-  return location;
+    return location;
 }
 
 export function getDOMLocation(historyState) {
-  const state = historyState || window.history.state || {};
-  const { pathname, search, hash } = window.location;
+    const state = historyState || window.history.state || {};
+    const { pathname, search, hash } = window.location;
 
-  let path = pathname + search + hash;
+    let path = pathname + search + hash;
 
-  return createLocation(path, state, key);
+    return createLocation(path, state, key);
 }
