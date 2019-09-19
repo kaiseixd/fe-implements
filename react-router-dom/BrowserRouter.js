8f99694ef6ac4741a97f14b9eb29a217f59bbdc4
kaiseixd <1050207224@qq.com>
2019-09-20 02:24:41 +0800

    add: BrowserRouter

---
+++ react-router-dom/BrowserRouter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Router } from '../react-router';
+import { BrowserHistory } from '../history';
+
+class BrowserRouter extends React.Component {
+    history = new BrowserHistory(this.props);
+
+    render() {
+        return <Router history={this.history} children={this.props.children} />;
+    }
+}
+
+export default BrowserRouter;
