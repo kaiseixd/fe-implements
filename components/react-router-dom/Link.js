6fa39a4d4a9298cfed4622be3214ea57eb7e80af
kaiseixd <1050207224@qq.com>
2019-10-05 16:12:01 +0800

    add: Route.js & Link.js

--- components/react-router-dom/Link.js
+++ components/react-router-dom/Link.js
@@ -1,2 +1,76 @@
 import React from 'react';
-import RouterContext from '../react-router/RouterContext';
+import { __RouterContext as RouterContext } from '@/react-router';
+import { resolveToLocation, normalizeToLocation } from "./utils/locationUtils";
+
+let { forwardRef } = React;
+
+function isModifiedEvent(event) {
+    return !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey);
+}
+
+const LinkAnchor = forwardRef(
+    ({ innerRef, navigate, onClick, ...rest }, forwardRef) => {
+        const { target } = rest;
+
+        return (
+            <a
+                {...rest}
+                ref={forwardRef || innerRef}
+                onClick={event => {
+                    try {
+                        onClick && onClick(event);
+                    } catch (e) {
+                        event.preventDefault();
+                        throw e;
+                    }
+
+                    if (
+                        !event.defaultPrevented && // onClick prevented default
+                        event.button === 0 && // ignore everything but left clicks
+                        (!target || target === "_self") && // let browser handle "target=_blank" etc.
+                        !isModifiedEvent(event) // ignore clicks with modifier keys
+                    ) {
+                        event.preventDefault();
+                        navigate();
+                    }
+                }}
+            />
+        );
+    }
+);
+
+const Link = forwardRef(
+    (
+        { component = LinkAnchor, replace, to, innerRef, ...rest },
+        forwardedRef
+    ) => {
+        return (
+            <RouterContext.Consumer>
+                {context => {
+                    const { history } = context;
+
+                    const location = normalizeToLocation(
+                        resolveToLocation(to, context.location),
+                        context.location
+                    );
+
+                    const href = location ? history.createHref(location) : "";
+
+                    return React.createElement(component, {
+                        ...rest,
+                        ref: forwardedRef || innerRef,
+                        href,
+                        navigate() {
+                            const location = resolveToLocation(to, context.location);
+                            const method = replace ? history.replace : history.push;
+
+                            method(location);
+                        }
+                    });
+                }}
+            </RouterContext.Consumer>
+        );
+    }
+);
+
+export default Link;
--- components/react-router/Route.js
+++ components/react-router/Route.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import RouterContext from "./RouterContext";
-import matchPath from "./matchPath";
+import RouterContext from './RouterContext';
+import matchPath from './matchPath';
 
 class Route extends React.Component {
     render() {
@@ -9,6 +9,31 @@ class Route extends React.Component {
                 {
                     context => {
                         const location = this.props.location || context.location;
+                        const match = matchPath(location.pathname, this.props);
+
+                        const props = { ...context, location, match };
+
+                        let { children, component, render } = this.props;
+
+                        return (
+                            <RouterContext.Provider value={props}>
+                                {
+                                    props.match
+                                        ? children
+                                            ? typeof children === 'function'
+                                                ? children(props)
+                                                : children
+                                            : component
+                                                ? React.createElement(component, props)
+                                                : render
+                                                    ? render(props)
+                                                    : null
+                                        : typeof children === 'function'
+                                            ? children(props)
+                                            : null
+                                }
+                            </RouterContext.Provider>
+                        )
                     }
                 }
             </RouterContext.Consumer>
